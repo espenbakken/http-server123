d1cf110d2f447e238d7fdc7c48ea1013bc5e0f6f
Espen Bakken <bakken.espen@gmail.com>
2020-09-30 16:24:41 +0200

    Both tests run red. Added comments to HttpServerTest.java

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -24,10 +24,26 @@ public class HttpServer {
     }
 
     private static void handleRequest(Socket socket) throws IOException {
-        String responseLine = HttpClient.readLine(socket);
-        System.out.println(responseLine);
+        String responseCode = "200";
 
-        String response = "HTTP/1.1 200 OK\r\n" +
+        //The first line of the incoming request is called the request line
+        String requestLine = HttpClient.readLine(socket);
+        System.out.println(requestLine);
+        // The requestLine consists of a verb (GET, POST), a request target and HTTP version
+        String requestTarget = requestLine.split(" ")[1];
+        // The request target can have a query string separated by ?
+        // For example /echo?status=404
+        int questionPos = requestTarget.indexOf('?');
+        if (questionPos != -1) {
+            String queryString = requestTarget.substring(questionPos+1);
+            // Here, more code is needed to handle other parameters than status
+            int equalsPos = queryString.indexOf('=');
+            // Each query parameter contains key=value
+            String parameterValue = queryString.substring(equalsPos+1);
+            responseCode = parameterValue;
+        }
+
+        String response = "HTTP/1.1 " + responseCode + " OK\r\n" +
                 "Content-Type: text/html; charset=utf-8\r\n" +
                 "Content-Length: 11\r\n" +
                 "\r\n" +
