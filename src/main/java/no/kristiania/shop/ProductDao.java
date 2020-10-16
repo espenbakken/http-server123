21f643befbc206b47297173492921c3537e1c9f2
Espen Bakken <bakken.espen@gmail.com>
2020-10-16 15:10:51 +0200

    Add and list information in Postgres

--- src/main/java/no/kristiania/shop/ProductDao.java
+++ src/main/java/no/kristiania/shop/ProductDao.java
@@ -1,10 +1,12 @@
 package no.kristiania.shop;
+
 import org.postgresql.ds.PGSimpleDataSource;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class ProductDao {
     public static void main(String[] args) throws SQLException {
@@ -13,12 +15,27 @@ public class ProductDao {
         dataSource.setUser("kristianiashop");
         dataSource.setPassword("sdlkgnslkawat");
 
-        Connection connection = dataSource.getConnection();
-        PreparedStatement statement = connection.prepareStatement("select * from products");
-        ResultSet rs = statement.executeQuery();
-        while(rs.next()) {
-            String productName = rs.getString("product_name");
-            System.out.println(productName);
+        System.out.println("Please enter product name:");
+        Scanner scanner = new Scanner(System.in);
+        String productName = scanner.nextLine();
+
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("INSERT INTO products (product_name) VALUES (?)")) {
+                statement.setString(1, productName);
+                statement.executeUpdate();
+            }
+        }
+
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("select * from products")) {
+                try (ResultSet rs = statement.executeQuery()) {
+                    while (rs.next()) {
+                        System.out.println(rs.getString("product_name"));
+                    }
+                }
+            }
         }
     }
 }
+
+
