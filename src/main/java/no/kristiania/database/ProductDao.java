c23e060991642ff2c9c3f57e61a0e4960887c694
Espen Bakken <bakken.espen@gmail.com>
2020-10-21 22:09:45 +0200

    Replaced with Shazo's comments

--- src/main/java/no/kristiania/database/ProductDao.java
+++ src/main/java/no/kristiania/database/ProductDao.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 import java.sql.Statement;
 
 public class ProductDao {
-
+    //data source is used for connecting to the actual data source
     private final DataSource dataSource;
 
     public ProductDao(DataSource dataSource) {
@@ -27,12 +27,13 @@ public class ProductDao {
                     "INSERT INTO products (product_name, age, last_name, email) values (?, ?, ?, ?)",
                     Statement.RETURN_GENERATED_KEYS
             )) {
+                //getter and setter method
                 statement.setString(1, product.getName());
                 statement.setDouble(2, product.getAge());
                 statement.setString(3, product.getLastName());
                 statement.setString(4, product.getEmail());
                 statement.executeUpdate();
-
+                //setting the keys to id
                 try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
                     generatedKeys.next();
                     product.setId(generatedKeys.getLong("id"));
@@ -42,6 +43,7 @@ public class ProductDao {
     }
 
     public Product retrieve(Long id) throws SQLException {
+        //connecting with a specific database and it gives information about the tables
         try (Connection connection = dataSource.getConnection()) {
             try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM products WHERE id = ?")) {
                 statement.setLong(1, id);
@@ -55,7 +57,7 @@ public class ProductDao {
             }
         }
     }
-
+    //creating a column of row in which the data will display/stored
     private Product mapRowToProduct(ResultSet rs) throws SQLException {
         Product product = new Product();
         product.setId(rs.getLong("id"));
