53359bc5ee456a384f8547ec6a91ccd4afc7d189
shazo4 <57667419+shazo4@users.noreply.github.com>
2020-10-21 14:12:19 +0200

    major changes in productDAO, and few changes in .gitignore

--- src/main/java/no/kristiania/database/ProductDao.java
+++ src/main/java/no/kristiania/database/ProductDao.java
@@ -4,65 +4,87 @@ package no.kristiania.database;
 import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class ProductDao {
 
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
     public ProductDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    public static void main(String[] args) throws SQLException {
-        PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/kristianiashop");
-        dataSource.setUser("kristianiashop");
-        dataSource.setPassword("sdlkgnslkawat");
-
-        ProductDao productDao = new ProductDao(dataSource);
-
-        System.out.println("Please enter product name:");
-        Scanner scanner = new Scanner(System.in);
-        String productName = scanner.nextLine();
-
-        Product product = new Product();
-        product.setName(productName);
-        productDao.insert(product);
-        for (Product p : productDao.list()) {
-            System.out.println(p);
-
-        }
-    }
-
-    public void insert(Product product) throws SQLException {
+    public void insert(Product projectMembers) throws SQLException {
         try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement("INSERT INTO products (product_name) VALUES (?)")) {
-                statement.setString(1, product.getName());
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO products (member_name) values (?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            )) {
+                statement.setString(1, projectMembers.getName());
                 statement.executeUpdate();
-            }
-        }
-    }
 
-    public List<Product> list() throws SQLException {
-        List<Product> products = new ArrayList<>();
-        try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement("select * from products")) {
-                try (ResultSet rs = statement.executeQuery()) {
-                    while (rs.next()) {
-                        Product product = new Product();
-                        product.setName(rs.getString("product_name"));
-                        products.add(product);
-                    }
+                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                    generatedKeys.next();
+
                 }
             }
         }
-        return products;
     }
+
+    private Product mapRowToProduct(ResultSet rs) throws SQLException {
+        Product projectMembers = new Product();
+        projectMembers.setName(rs.getString("member_name"));
+        return projectMembers;
+    }
+
+    public List<Product> list() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM projectMember")) {
+                try (ResultSet rs = statement.executeQuery()) {
+                    List<Product> projectMember = new ArrayList<>();
+                    while (rs.next()) {
+                        projectMember.add(mapRowToProduct(rs));
+                    }
+                    return projectMember;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+
+        Properties properties = new Properties();
+        try (FileReader fileReader = new FileReader("pgr203.properties")){
+        properties.load(fileReader);} catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl(properties.getProperty("dataSource.url"));
+        dataSource.setUser(properties.getProperty("dataSource.username"));
+        // TODO: database passwords should never be checked in!
+        dataSource.setPassword(properties.getProperty("dataSource.password"));
+
+        ProductDao productDao = new ProductDao(dataSource);
+
+        System.out.println("What's the name of the new projectMember");
+        Scanner scanner = new Scanner(System.in);
+
+        Product projectMember = new Product();
+        projectMember.setName(scanner.nextLine());
+
+        productDao.insert(projectMember);
+        System.out.println(productDao.list());
+    }
+
 }
