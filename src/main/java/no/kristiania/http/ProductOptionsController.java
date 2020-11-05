1adb5170cfdf61ac08f9bc12d1ce383861211d6f
Espen Bakken <bakken.espen@gmail.com>
2020-11-05 19:14:27 +0100

    ProductOptionsControllerTest passes

--- src/main/java/no/kristiania/http/ProductOptionsController.java
+++ src/main/java/no/kristiania/http/ProductOptionsController.java
@@ -1,5 +1,6 @@
 package no.kristiania.http;
 
+import no.kristiania.database.Member;
 import no.kristiania.database.MemberDao;
 
 import java.io.IOException;
@@ -7,7 +8,10 @@ import java.net.Socket;
 import java.sql.SQLException;
 
 public class ProductOptionsController implements HttpController {
+    private MemberDao memberDao;
+
     public ProductOptionsController(MemberDao memberDao) {
+        this.memberDao = memberDao;
     }
 
     @Override
@@ -21,7 +25,13 @@ public class ProductOptionsController implements HttpController {
         clientSocket.getOutputStream().write(response.getBytes());
     }
 
-    public String getBody() {
-        return "<option>A</option><option>B</option>";
+    public String getBody() throws SQLException {
+        String body = "";
+        for (Member member : memberDao.list()) {
+            body += "<option value=" + member.getId() + ">" + member.getName() + "</option>";
+        }
+
+
+        return body;
     }
 }
